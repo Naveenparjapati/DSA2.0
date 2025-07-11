06490f605e6c96397e1ea7c254eea634913c2c66
Naveen-kkkk <naveenkumarbalsamand123@gmail.com>
2025-07-11 18:56:05 +0530

    Daimond pattern

---
+++ src/Sir_Program/Dimand_Pattern.java
@@ -0,0 +1,40 @@
+package Sir_Program;
+
+public class Dimand_Pattern {
+
+    public static void main(String[] args) {
+        int n = 5;  // The number of rows in the top half (can be changed)
+        
+        // Print the top half of the diamond
+        for (int i = 1; i <= n; i++) {
+            // Print spaces
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+            
+            // Print stars
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+            
+            // Move to the next line after each row
+            System.out.println();
+        }
+
+        // Print the bottom half of the diamond
+        for (int i = n - 1; i >= 1; i--) {
+            // Print spaces
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+            
+            // Print stars
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+            
+            // Move to the next line after each row
+            System.out.println();
+        }
+    }
+}
