07db688b485c72dd2ad9847954719bc2c1aac150
Naveen-kkkk <naveenkumarbalsamand123@gmail.com>
2025-07-11 18:50:53 +0530

    integer to binary

---
+++ src/Sir_Program/integer_to_binary.java
@@ -0,0 +1,10 @@
+package Sir_Program;
+
+public class integer_to_binary {
+
+	    public static void main(String[] args) {
+	        int number = 13;  // Example integer
+	        String binaryString = Integer.toBinaryString(number);  // Convert integer to binary string
+	        System.out.println("Integer " + number + " as binary: " + binaryString);
+	    }
+	}
