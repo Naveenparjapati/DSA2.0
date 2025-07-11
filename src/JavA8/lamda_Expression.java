0b727cf98f14553a0d17c953a26dd51e3e57d1cf
Naveen-kkkk <naveenkumarbalsamand123@gmail.com>
2025-07-11 10:57:36 +0530

    lambda expression

---
+++ src/JavA8/lamda_Expression.java
@@ -0,0 +1,19 @@
+package JavA8;
+
+
+interface rectangle{
+	double area();
+}
+
+
+public class lamda_Expression {
+public static void main(String[] args)
+{
+	rectangle r=()->{
+		double res=12.5*3;
+		return res;
+	};
+	double w=r.area();
+	System.out.print(w);
+}
+}
