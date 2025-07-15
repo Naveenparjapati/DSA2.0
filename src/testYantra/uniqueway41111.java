d6b4e79a294b6971f7e3e22d9d8de67186e37eb1
Naveen-kkkk <naveenkumarbalsamand123@gmail.com>
2025-07-15 15:58:11 +0530

    todat int quistion

---
+++ src/testYantra/uniqueway41111.java
@@ -0,0 +1,28 @@
+package testYantra;
+
+public class uniqueway41111 
+{
+	
+    public static int countWay(int n)
+    {
+	 //base case
+	 if(n==0|| n==1)
+	 {
+		 return 1;
+	 }
+	 
+	 int[] arr=new int[n+1];
+	 arr[0]=1;
+	 arr[1]=1;
+	 for(int i=2;i<=n;i++)
+	 {
+		 arr[i]=arr[i-1]+arr[i-2];
+	 }
+	 return arr[n];
+ }
+   public static void main(String[] args)
+   {
+	 int n=4;
+	 System.out.println("number of ways  "+countWay(n));
+	}
+}
