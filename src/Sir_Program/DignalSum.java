a9d262fd291cab46195bca0c945eb69e8b34cb27
Naveen-kkkk <naveenkumarbalsamand123@gmail.com>
2025-07-16 07:38:21 +0530

    DignalSum

--- src/Sir_Program/DignalSum.java
+++ src/Sir_Program/DignalSum.java
@@ -1,5 +1,19 @@
 package Sir_Program;
 
 public class DignalSum {
-
+	 public static void main(String[] args)
+	   {
+		 int[][] arr={{1,2,3},
+				      {4,5,6},
+				      {7,8,9}};
+		 int sum=0;
+		 
+		 for(int i=0;i<arr.length;i++)
+		 {
+			 sum=sum+arr[i][i];
+		 }
+		 
+		 System.out.print(sum);
+	   }
+	 
 }
