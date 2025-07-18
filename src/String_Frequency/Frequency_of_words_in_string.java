27e34c5eb205de527c684ee2158e77e06fa8f5e0
Naveen-kkkk <naveenkumarbalsamand123@gmail.com>
2025-07-18 21:20:34 +0530

    frequeny of sentence words

--- src/String_Frequency/Frequency_of_words_in_string.java
+++ src/String_Frequency/Frequency_of_words_in_string.java
@@ -1,5 +1,22 @@
 package String_Frequency;
 
-public class Frequency_of_words_in_string {
+import java.util.HashMap;
+import java.util.Map;
 
+public class Frequency_of_words_in_string {
+	public static void main(String[] args) {
+        String str = "hi java hi hello";
+        String[] stA=str.split(" ");
+        Map<String, Integer> freqMap = new HashMap<>();
+
+        for (String ch :stA) {
+                freqMap.put(ch, freqMap.getOrDefault(ch, 0) + 1);
+        }
+
+        // Print the frequencies
+        for (Map.Entry<String, Integer> entry : freqMap.entrySet()) {
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
+
+ }
 }
