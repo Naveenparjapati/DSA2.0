1c1f0fe2170bb803712c53425ae4a17af1fa7756
Naveen-kkkk <naveenkumarbalsamand123@gmail.com>
2025-07-20 22:19:28 +0530

    heap 1

---
+++ src/Heap_Data_Structure/pmg1.java
@@ -0,0 +1,22 @@
+package Heap_Data_Structure;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class pmg1 {
+	public static void main(String args[]) {
+	   // PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		   
+		pq.add(3); // O(logn)
+	    pq.add(4);
+	    pq.add(1);
+	    pq.add(7);
+
+	    while(!pq.isEmpty()) {
+	        System.out.println(pq.peek()); // O(1)
+	        pq.remove(); // O(logn)
+	    }
+	}
+
+}
