247d6d4f0e5ca66370ac08928eb2a3d45c7be0bd
Naveen-kkkk <naveenkumarbalsamand123@gmail.com>
2025-07-23 19:23:12 +0530

    minimum absolute diffrence

---
+++ src/greedyAlgo/Minimum_absolute_difrent.java
@@ -0,0 +1,22 @@
+package greedyAlgo;
+
+import java.util.Arrays;
+
+public class Minimum_absolute_difrent {
+	 public static void main(String[] args) {
+	      int[] A={4,1,8,7};
+	      int[] B={2,3,6,5};
+	      
+	      Arrays.sort(A);
+	      Arrays.sort(B);
+	      
+	      int minDiff=0;
+	      for(int i=0;i<A.length;i++)
+	      {
+	        minDiff +=Math.abs(A[i]-B[i]);
+	      }
+	      
+	      System.out.print("Minimum abslute diffrence "+minDiff);
+	      
+	  }
+}
